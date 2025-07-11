7cdffaad87a126e99876e8da8f33602727814227
zerotohero200222 <aebharath17@gmail.com>
2025-07-11 19:53:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+# Generate a random string to make the Lambda function name unique
+resource "random_string" "suffix" {
+  length  = 6
+  upper   = false
+  special = false
+}
+
+# IAM Assume Role Policy Document for Lambda
 data "aws_iam_policy_document" "assume_role" {
   statement {
     effect = "Allow"
@@ -9,26 +17,23 @@ data "aws_iam_policy_document" "assume_role" {
   }
 }
 
+# IAM Role for Lambda Execution
 resource "aws_iam_role" "iam_for_lambda" {
   name               = "iam_for_lambda"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
-resource "random_string" "suffix" {
-  length  = 6
-  upper   = false
-  special = false
-}
-
+# Package the Lambda function into a ZIP file
 data "archive_file" "lambda" {
   type        = "zip"
   source_file = "lambda.js"
   output_path = "lambda_function_payload.zip"
 }
 
+# Lambda Function Resource
 resource "aws_lambda_function" "test_lambda" {
   function_name = "${var.lambda_function_name_prefix}-${random_string.suffix.result}"
-  filename      = "lambda_function_payload.zip"
+  filename      = data.archive_file.lambda.output_path
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "index.test"
   runtime       = "nodejs18.x"
@@ -39,5 +44,10 @@ resource "aws_lambda_function" "test_lambda" {
     variables = {
       foo = "bar"
     }
+  }
+
+  tags = {
+    Environment = "dev"
+    Project     = "LambdaDeployment"
   }
 }
