10becb4f79b24cb6cc4494dec7ef2dfbffe1691b
zerotohero200222 <aebharath17@gmail.com>
2025-07-13 19:11:15 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,29 @@
-variable "lambda_function_name_prefix" {
-  description = "Prefix for the Lambda function name"
+variable "aws_region" {
+  description = "AWS region"
   type        = string
 }
 
-variable "aws_region" {
-  description = "AWS region to deploy to"
+variable "lambda_function_name_prefix" {
+  description = "Prefix for Lambda function name"
+  type        = string
+}
+
+variable "lambda_handler" {
+  description = "Lambda handler"
+  type        = string
+}
+
+variable "lambda_runtime" {
+  description = "Lambda runtime"
+  type        = string
+}
+
+variable "lambda_source_file" {
+  description = "Lambda source JS file"
+  type        = string
+}
+
+variable "lambda_output_path" {
+  description = "Path to output Lambda ZIP"
   type        = string
 }
